880bad181848cd605ce6407aead39eb54ee03d90
Yahya Faroun <yahyafaroun@gmail.com>
2020-09-14 13:51:15 +0200

    reload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Faq from './Faq';
-import FaqItem from './FaqItem';
+
 
 function App() {
   return (
     <div className="App">
-      <Faq/>
+      <Faq />
     </div>
   );
 }
